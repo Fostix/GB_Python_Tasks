d999f97ced423a623955369eeee87a7841b101e3
Albert <AccretionDiskThat@Gmail.com>
2022-02-16 20:32:20 +0500

    maked_task_18_random

--- Lesson_2/Task_18_Random.py
+++ Lesson_2/Task_18_Random.py
@@ -1,10 +1,19 @@
-# Реализовать алгоритм перемешивания списка. 
+# Реализовать алгоритм перемешивания списка.
 
 
-import random
-
-
-numbers = [1,2,33,4,5,65,7,8,9]
+import time, random
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 random.shuffle(numbers)
 print(numbers)
+
+
+# for i in range(len(numbers)):
+#     mixer = str(time.time())
+#     mixer = int(mixer.replace('.', '')) % 100
+
+#     while mixer > len(numbers) - 1:
+#         mixer //= 2
+#     numbers[i], numbers[mixer] = numbers[mixer], numbers[i]
+
+# print(numbers)
